fcbc9568f41e5af3aa6cef39f2bb70c84e32e683
Farney Jiménez Toro <Fjimenezt@intergrupo.com>
2021-11-09 16:13:10 -0500

    Add routerLInk in AccountSetting view

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from 'src/app/services/settings.service';
 
 @Component({
   selector: 'app-account-settings',
@@ -6,19 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styles: [],
 })
 export class AccountSettingsComponent implements OnInit {
-  
-  public linkTheme = document.querySelector('#theme');
 
-  constructor() {}
+  constructor( private settingsService: SettingsService)  { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.settingsService.checkCurrentTheme();
+  }
 
   changeTheme(theme: string) {
-    const url = `./assets/css/colors/${ theme }.css`;
-
-    this.linkTheme.setAttribute('href', url);
-    localStorage.setItem('theme', url);
-    // console.log(url);
+    this.settingsService.changeTheme( theme );
     
   }
 }
--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../services/settings.service';
 
 @Component({
   selector: 'app-pages',
@@ -8,15 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagesComponent implements OnInit {
 
-  public linkTheme = document.querySelector('#theme');
-
-
-  constructor() { }
+  constructor( private readonly settingsService: SettingsService) { }
 
   ngOnInit(): void {
     //  ./assets/css/colors/purple-dark.css
-    const url = localStorage.getItem('theme') || './assets/css/colors/purple-dark.css';
-    this.linkTheme.setAttribute('href', url);
   }
-
 }
