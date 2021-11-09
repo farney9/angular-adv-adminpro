e2ab4b427b5f8f3286660a5ce164ef8a9351455c
Farney Jiménez Toro <Fjimenezt@intergrupo.com>
2021-11-09 16:57:30 -0500

    Add SidebarService

--- src/app/shared/sidebar/siderbar.component.ts
+++ src/app/shared/sidebar/siderbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SidebarService } from 'src/app/services/sidebar.service';
 
 @Component({
   selector: 'app-siderbar',
@@ -8,7 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SiderbarComponent implements OnInit {
 
-  constructor() { }
+  menuItems: any[];
+
+  constructor( private sidebarService: SidebarService) { 
+    this.menuItems = sidebarService.menu;
+    console.log(this.menuItems);
+    
+  }
 
   ngOnInit(): void {
   }
