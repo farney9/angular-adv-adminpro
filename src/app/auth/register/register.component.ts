510686a0aee440e855c44fb37cfa53d4052b57ca
Farney Jiménez Toro <Fjimenezt@intergrupo.com>
2022-02-17 16:06:33 -0500

    create reactive form

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,  } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent  {
+  
+  registerForm = this.fb.group({
+    name: ['Farney', Validators.required],
+    email:  ['tategod772@bepureme.com', Validators.required],
+    password:  ['123456', Validators.required],
+    passwordConfirm:  ['123456', Validators.required],
+    ischeckedTermsOfUse: [false, Validators.required]
+  });
 
-  constructor() { }
+  constructor ( private readonly fb: FormBuilder, ) { }
 
-  ngOnInit(): void {
+  buildRegisterForm() {
+    console.log(this.registerForm.value);
   }
-
 }
