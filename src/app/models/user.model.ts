4b5d77b38b1fb0def0afe5df81e69e83e9d9bf4a
Farney Jiménez Toro <Fjimenezt@intergrupo.com>
2022-02-17 13:52:51 -0500

    add user model

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,16 @@
+
+
+export class User {
+    
+    constructor(
+        public name:   string,
+        public email:  string,
+        public password?:  string,
+        public image?:  string,
+        public google?: boolean,
+        public role?:   string,
+        public uid?:    string,
+    ) {} 
+
+    
+}
