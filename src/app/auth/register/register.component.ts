1cafa004c97e6f107ae7a4012f4600fbbf7c3f90
Farney Jiménez Toro <Fjimenezt@intergrupo.com>
2022-02-18 20:47:04 -0500

    validate same passwords and refactor validations

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,24 +1,61 @@
-import { Component,  } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component, } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent  {
+export class RegisterComponent {
+  //formato para email válido
+  emailPattern = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
   
-  registerForm = this.fb.group({
-    name: ['Farney', Validators.required],
-    email:  ['tategod772@bepureme.com', Validators.required],
-    password:  ['123456', Validators.required],
-    passwordConfirm:  ['123456', Validators.required],
-    ischeckedTermsOfUse: [false, Validators.required]
-  });
+  isFormSubmited: boolean = false;
+  registerForm: FormGroup;
 
-  constructor ( private readonly fb: FormBuilder, ) { }
+  constructor(private readonly fb: FormBuilder,) {
+    this.registerForm = this.fb.group({
+      name:                new FormControl('a', Validators.required),
+      email:               new FormControl('a@b.com', [Validators.required, Validators.email, Validators.pattern(this.emailPattern)]),
+      password:            new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      passwordConfirm:     new FormControl(null, Validators.required),
+      isCheckedTermsOfUse: new FormControl(null, Validators.required)
+    },
+    {
+       validators: this.mustMatch('password', 'passwordConfirm')
+    })
+  }
 
-  buildRegisterForm() {
+  // funcion f para tener accesso a los controles y sus propiedades desde el HTML
+  get f() { return this.registerForm.controls }
+
+  mustMatch(controlName: string, matchingControlName: string) {
+    return(formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+        return
+      }
+
+      if (control.value !== matchingControl.value ) {
+        matchingControl.setErrors({mustMatch: true});
+      } else {
+        matchingControl.setErrors(null);
+      }
+    }
+  }
+
+
+  onSubmit() {
+    this.isFormSubmited = true;
     console.log(this.registerForm.value);
+
+    if (this.registerForm.valid) {
+      console.log('posteando formulario');
+    } else {
+      console.log('formulario no es válido');
+
+    }
   }
 }
