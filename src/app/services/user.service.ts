c9435821701033b4dff7d581d6613748c71a380b
Farney Jiménez Toro <Fjimenezt@intergrupo.com>
2022-03-08 18:51:58 -0500

    Add token to Local Storage

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from "rxjs/operators";
 import { environment } from 'src/environments/environment';
 import { UserLoginRequest, UserRegisterRequest } from '../auth/models/request-user.model';
 
@@ -12,9 +13,19 @@ export class UserService {
 
   add(body: UserRegisterRequest) {
     return this.http.post(`${environment.apiUrl}/usuario`, body)
+    .pipe(
+      tap( (res: any ) => { 
+        localStorage.setItem('token', res.token)
+      })
+    );
   }
 
   login(body: UserLoginRequest) {
     return this.http.post(`${environment.apiUrl}/login`, body)
+      .pipe(
+        tap( (res: any ) => { 
+          localStorage.setItem('token', res.token)
+        })
+      );
   }
 }
