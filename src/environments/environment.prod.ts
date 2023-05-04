57ecd00b1865dde6660e0c852ec1c40ff0d8cdb1
Farney <farney9@gmail.com>
2023-05-03 20:06:57 -0500

    fix eviroment production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'admin-pro-backend-production-f660.up.railway.app:6320/api'
+  apiUrl: 'admin-pro-backend-production-f660.up.railway.app:5905/api'
   // apiUrl: 'http://localhost:3000/api'
 };
